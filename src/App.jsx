ce66b22413c423243b5e255ee718a32a7b1717c0
TechShri027 <shrishtibansal2705@gmail.com>
2025-03-20 21:26:14 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 import './App.css'
+import * as React from 'react';
+import { grey } from '@mui/material/colors';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+    <div className="card" >
+      <form action="">
+      <h2>Welcome to</h2>
+      <h1>Design School</h1>
+<div className="buttons">
+<button>Login with Google</button>
+      <br />
+      <button>Login with Facebook</button>
+      <TextField id="filled-basic" label="Email" variant="filled" sx={{width:"80%",marginTop:"20px",fontWeight:"200px"}} />
+     
+      <TextField
+          id="filled-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="filled"
+          sx={{width:"80%",marginTop:"20px" , fontWeight:"200px"}} 
+        />
+
+       <div className="inp">
+        <li><input type="checkbox" name="" id="checkbox" style={{backgroundColor:"gray"}} /><span>Remember me </span></li>
+        <li style={{color:"rgb(98, 88, 220)"}}>Forgot password?</li>
+        </div> 
+
+        <Button variant="contained" sx={{backgroundColor:'rgb(98, 88, 220)', marginTop:"20px"}}>Login</Button>
+
+       <div style={{fontSize:"12px",marginTop:"20px"}}>Don't have an account?<span style={{color:"rgb(98,88,220)"}}>Register</span></div>
+    </div>
+
+     
+
+
+
+      </form>
+     
+
+
+    </div>
     </>
   )
 }
